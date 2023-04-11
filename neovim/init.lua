64c9ed8a846e55e7e8a014b0c3c5468feb6a9338
Igor Glajcher <kasetonix@gmail.com>
2023-04-11 22:01:27 +0200

    [11.04.23] Modified neovim config

--- neovim/init.lua
+++ neovim/init.lua
@@ -7,9 +7,9 @@
 --
 -- TODO:
 -- * Look through keybinds, remove unused
+-- ~ Add TodoTelescope shortcut
 -- * Configure trouble.nvim
 -- * Reduce vim.cmd usage
--- ~ Add TodoTelescope shortcut
 
 -- Set <space> as the leader key
 vim.g.mapleader = ' '
@@ -233,11 +233,6 @@ pcall(require('telescope').load_extension, 'fzf')
 
 -- Telescope shortcuts
 vim.keymap.set('n', '<leader>sf', require('telescope.builtin').find_files, { desc = '[S]earch [F]iles' })
-vim.keymap.set('n', '<leader>sh', require('telescope.builtin').help_tags, { desc = '[S]earch [H]elp' })
-vim.keymap.set('n', '<leader>sw', require('telescope.builtin').grep_string, { desc = '[S]earch current [W]ord' })
-vim.keymap.set('n', '<leader>sg', require('telescope.builtin').live_grep, { desc = '[S]earch by [G]rep' })
-vim.keymap.set('n', '<leader>sd', require('telescope.builtin').diagnostics, { desc = '[S]earch [D]iagnostics' })
-
 vim.keymap.set('n', '<leader>?', require('telescope.builtin').oldfiles, { desc = '[?] Find recently opened files' })
 vim.keymap.set('n', '<leader><space>', require('telescope.builtin').buffers, { desc = '[ ] Find existing buffers' })
 vim.keymap.set('n', '<leader>/', function()
