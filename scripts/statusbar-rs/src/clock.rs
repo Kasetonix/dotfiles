0bf7545c3cde7dbe293d7a1aceef114fc0e51bdf
Igor Glajcher <kasetonix@gmail.com>
2023-08-01 16:53:52 +0200

    [01.08.23] Removed statusbar-rs from scripts as it was moved to a separate repository

--- scripts/statusbar-rs/src/clock.rs
+++
@@ -1,9 +0,0 @@
-use std::process::Command;
-
-pub fn draw_time() -> String {
-    /* Getting the time */
-    let output = Command::new("date").arg("+%H:%M").output().expect("date couldn't be run.");
-    let time = String::from_utf8(output.stdout).expect("Couldn't capture the date output."); 
-    /* Returning the trimmed time */
-    time.trim().to_string()
-}
