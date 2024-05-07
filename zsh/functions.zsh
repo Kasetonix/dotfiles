3403b0622a367d953fe627588054232893f551c7
Kasetonix <kasetonix@gmail.com>
2024-05-07 14:02:48 +0200

    [07.05.2024] Added functions to zsh to download yt vids easily through yt-dlp and fzf + to fuzzy cd; Modified rangers scope.sh

--- zsh/functions.zsh
+++ zsh/functions.zsh
@@ -20,3 +20,17 @@ function gcomp() {
 function gcom() {
     git commit -m "$(date '+[%d.%m.%Y]') $1"
 }
+
+function fzcd() {
+    cd $(find . -mindepth 1 -type d \( -name '.*' -prune -o -print \) | fzf)
+}
+
+function fzcdh() {
+    cd $(find . -mindepth 1 -type d | fzf)
+}
+
+
+function yt-dl() {
+    dest="$(find . -mindepth 1 -type d \( -name '.*' -prune -o -print \) | fzf)"
+    yt-dlp -f "bestvideo[ext=mp4]+bestaudio[ext=m4a]/best[ext=mp4]/best" --no-simulate --print "pre_process:title" -o "%(title)s.%(ext)s" --restrict-filenames --progress --extractor-args "youtube:player_client=web" -N 3 -P "$dest" "$1"
+}
