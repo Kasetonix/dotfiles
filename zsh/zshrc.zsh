b13afffa4c0b9c292acdb3bc8d27860f29ea46b5
Kasetonix <kasetonix@gmail.com>
2024-06-20 15:45:48 +0200

    [20.06.2024] Changed the zsh prompt and font in firefox

--- zsh/zshrc.zsh
+++ zsh/zshrc.zsh
@@ -30,11 +30,9 @@ source "$ZDOTDIR/source-functions.zsh"
 autoload -U colors && colors
 setopt PROMPT_SUBST
 source-plugin-local "short-pwd.zsh" # Sourcing the function for shortening the pwd
-PS1='%{$fg_bold[green]%}$(short-pwd) %(!.%{$fg_bold[red]%}.%{$fg_bold[cyan]%})➜ %{$reset_color%}%{$fg_bold[white]%}'
-# PS1='%{$fg_bold[green]%}$(short-pwd) %(!.%{$fg_bold[red]%}.%{$fg_bold[cyan]%}) %{$reset_color%}%{$fg_bold[white]%}'
-
-PS2='%{$fg_bold[cyan]%}$(short-pwd-len) ➜ %{$reset_color%}%{$fg_bold[white]%}'
-# PS2='%{$fg_bold[cyan]%}$(short-pwd-len)  %{$reset_color%}%{$fg_bold[white]%}'
+# PS1='%{$fg_bold[green]%}$(short-pwd) %(?.%{$fg_bold[cyan]%}.%{$fg_bold[red]%})➜ %{$reset_color%}%{$fg_bold[white]%}'
+PS1='%{$fg_bold[green]%}$(short-pwd) %(!.%{$fg_bold[yellow]%}.%{$fg_bold[cyan]%})%(?..%{$fg_bold[red]%})%(!.*.➜) %{$reset_color%}%{$fg_bold[white]%}'
+PS2='$(short-pwd-len) %(!.%{$fg_bold[yellow]%}.%{$fg_bold[cyan]%})%(!.*.➜) %{$reset_color%}%{$fg_bold[white]%}'
 preexec() { printf "\e[0m"; } # resetting the formatting before the command executes
 
 # Sourcing configs
