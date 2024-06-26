ed4c63a0b1f0fc7c63dbd52f3bb8280ee8f3ac7d
Kasetonix <kasetonix@gmail.com>
2024-06-26 22:32:38 +0200

    [26.06.2024] Removed annoying scope start/end underlines from ibl in neovim

--- nvim/lua/plugin/themes.lua
+++ nvim/lua/plugin/themes.lua
@@ -62,6 +62,7 @@ require("transparent").setup {
 require('ibl').setup {
   indent = { char = "│" },
   -- indent = { char = "┊" },
+  scope = { show_start = false, show_end = false },
 }
 
 -- dressing - changes the look of subwindows
