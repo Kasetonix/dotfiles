64ecb53ed067c4d91ec8aa9d335d3e445f3ace64
Kasetonix <kasetonix@gmail.com>
2024-09-29 12:18:40 +0200

    [29.09.2024] Added rifle configuration; ranger now opens gifs in mpv

--- nvim/lua/plugin/themes.lua
+++ nvim/lua/plugin/themes.lua
@@ -8,6 +8,7 @@ require('lualine').setup {
   options = {
     component_separators = { left = '', right = ''},
     section_separators = { left = '', right = ''},
+    -- section_separators = { left = '', right = ''},
   },
   extensions = {'nvim-tree'},
 
