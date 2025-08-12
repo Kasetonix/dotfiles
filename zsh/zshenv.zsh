f1906ae59f2b8c36d8f4ce4e3452896c04d78bba
kasetonix <kasetonix@gmail.com>
2025-08-12 12:30:09 +0200

    [12.08.2025] Changed the formatting for default options for fzf in zsh

--- zsh/zshenv.zsh
+++ zsh/zshenv.zsh
@@ -5,13 +5,47 @@ RANGER_LOAD_DEFAULT_RC="false"
 ZSH_AUTOSUGGEST_HIGHLIGHT_STYLE="fg=#444b6a"
 
 # FZF default options
-fzf_default_opts+=(
-    "--info=inline"
-    "--border=rounded"
-    "--height=50%"
-    "--multi"
-    "--prompt='⊳ '"
-    "--pointer='➜ '"
-    "--marker=''"
-    "--color='hl:cyan,hl+:blue,bg+:-1,pointer:cyan,marker:red,gutter:-1'"
-) export FZF_DEFAULT_OPTS=$(printf '%s\n' "${fzf_default_opts[@]}")
+export FZF_DEFAULT_OPTS="$FZF_DEFAULT_OPTS \
+    --highlight-line \
+    --info=inline \
+    --ansi \
+    --layout=reverse \
+    --border=rounded \
+    --height=~50% \
+    --multi \
+    --prompt='⊳ ' \
+    --pointer='➜ ' \
+    --marker='·' \
+    --color=bg+:-1 \
+    --color=bg:-1 \
+    --color=border:bright-black \
+    --color=fg+:bright-white \
+    --color=fg:white \
+    --color=gutter:-1 \
+    --color=header:yellow \
+    --color=hl+:cyan \
+    --color=hl:cyan \
+    --color=info:yellow \
+    --color=marker:red \
+    --color=pointer:cyan \
+    --color=prompt:cyan \
+    --color=separator:bright-black \
+    --color=query:bright-white \
+    --color=scrollbar:cyan \
+    --color=spinner:cyan \
+"
+
+# CTRL-Y to copy the command into clipboard using wl-copy
+export FZF_CTRL_R_OPTS="
+  --bind 'ctrl-y:execute-silent(echo -n {2..} | wl-copy)+abort'
+  --color header:italic"
+
+export FZF_CTRL_T_OPTS="
+  --walker-skip .git,node_modules,target"
+
+# Print tree structure in the preview window
+export FZF_ALT_C_OPTS="
+  --walker-skip .git,node_modules,target
+  --preview 'tree -C {}'"
+
+
