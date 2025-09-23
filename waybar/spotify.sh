33d4d8fdf85b42b0369adb575a86b7e16d60342d
kasetonix <kasetonix@gmail.com>
2025-09-23 20:49:44 +0200

    [23.09.2025] Changed the waybar styling of spotify and powermenu modules

--- waybar/spotify.sh
+++ waybar/spotify.sh
@@ -2,5 +2,5 @@
 lim=$1
 [ $(playerctl -p spotify status) = 'Paused' ] && status='' || status=''
 title="$(playerctl -p spotify metadata xesam:title)"
-[ ${#title} -ge $lim ] && title="$(echo ${title:0:$((lim-1))}…)"
-echo "$status  ⟨$title⟩"
+[ ${#title} -gt $lim ] && title="$(echo ${title:0:$((lim-1))}…)"
+echo "$status   ⟨$title⟩"
