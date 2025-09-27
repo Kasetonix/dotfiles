c84c30ecaae6ce3abd14df72cb819640c206a65b
kasetonix <kasetonix@gmail.com>
2025-09-27 19:15:50 +0200

    [27.09.2025] Added a default value of $lim in spotify.sh file

--- waybar/spotify.sh
+++ waybar/spotify.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 lim=$1
+[ -z lim ] || lim="32"
 [ $(playerctl -p spotify status) = 'Paused' ] && status='' || status=''
 title="$(playerctl -p spotify metadata xesam:title)"
-[ ${#title} -gt $lim ] && title="$(echo ${title:0:$((lim-1))}…)"
+[ "${#title}" -gt "$lim" ] && title="$(echo ${title:0:$((lim-1))}…)"
 echo "$status   ⟨$title⟩"
