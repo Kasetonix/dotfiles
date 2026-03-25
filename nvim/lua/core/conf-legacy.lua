a5b23b06249218467f66b3779cfc591bf5480fa1
Igor Glajcher <kasetonix@gmail.com>
2026-03-25 17:59:35 +0100

    [25.03.2026] Added nvim keybind to swap neighbouring lines.

--- nvim/lua/core/conf-legacy.lua
+++ nvim/lua/core/conf-legacy.lua
@@ -7,6 +7,8 @@ vim.cmd [[
   set noswapfile " Doesn't generate swap file
   " remaps
   nnoremap <silent> <C-l> :set nu!<CR>
+  nnoremap <silent> <C-Up> :m-2<CR>
+  nnoremap <silent> <C-Down> :m+1<CR>
 ]]
 
 -- vim: ts=2 sts=2 sw=2 et
