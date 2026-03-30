5c588fe30a98c90d53f170ed12ba60bb0caa0631
Igor Glajcher <kasetonix@gmail.com>
2026-03-30 18:45:08 +0200

    [30.03.2026] Added rules to spider.nvim to ignore braces.

--- nvim/lua/plugin/functions.lua
+++ nvim/lua/plugin/functions.lua
@@ -96,6 +96,13 @@ require('autoclose').setup {
 }
 
 --- /// NVIM-SPIDER ///
+require('spider').setup {
+  customPatterns = {
+    patterns = { "%(", "%)", "%>", "%<", "%[", "%]" },
+    overrideDefault = false,
+  },
+}
+
 vim.keymap.set({ "n", "o", "x" }, "w", "<cmd>lua require('spider').motion('w')<CR>")
 vim.keymap.set({ "n", "o", "x" }, "e", "<cmd>lua require('spider').motion('e')<CR>")
 vim.keymap.set({ "n", "o", "x" }, "b", "<cmd>lua require('spider').motion('b')<CR>")
